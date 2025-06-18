30981825421f01b15d92d21518e24fc0e51b3e46
AmineSkhiri <amiine.skhiri@gmail.com>
2025-06-18 19:27:38 +0100

    app.js Updated, pushing code to deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("✅ Hello from ECS App Updated!");
+  res.send("✅ Hello from ECS App Updated for Eks and ECS!");
 });
 
 app.listen(PORT, () => {
